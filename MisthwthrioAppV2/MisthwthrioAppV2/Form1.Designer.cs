a88790ed4e53545d8942f8b9287865f787dc03f5
ErrikosKalIT134099 <44244924+ErrikosKalIT134099@users.noreply.github.com>
2021-09-09 22:42:19 +0300

    added icon // changed name of branch

--- MisthwthrioAppV2/MisthwthrioAppV2/Form1.Designer.cs
+++ MisthwthrioAppV2/MisthwthrioAppV2/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace MisthwthrioAppV2
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,15 +29,17 @@ namespace MisthwthrioAppV2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.SuspendLayout();
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FloralWhite;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(841, 880);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
             this.Text = "Φόρμα Μισθωτήριου";
             this.ResumeLayout(false);
 
